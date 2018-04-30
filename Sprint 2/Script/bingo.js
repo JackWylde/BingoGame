cc3b327741ff108d74f5af80e74511a378b45384
JackWylde <36413725+JackWylde@users.noreply.github.com>
2018-04-30 18:07:02 +0100

    Update bingo.js

--- Sprint 2/Script/bingo.js
+++ Sprint 2/Script/bingo.js
@@ -2,7 +2,7 @@
 
 window.onload = initGame; //Calls the initGame function when the browser is opened
 
-function initGame() //the purpose of this function is to hold a nested loop that creates a random number between 1-75 for every individual cell in the bingo game.
+function initGame() //function that calls the functions and variables that need to be loaded when the page opens
 {
 	bingoBalls = shuffleBalls(bingoCall(1,78)); ////globally declares a variable with the parameters needed for the bingoCall function, as well as giving shuffleBalls what it needs to shuffle
 }
